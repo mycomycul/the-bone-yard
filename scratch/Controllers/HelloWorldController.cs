4dafbdbf191dfa0c05d383705cc171eafd815874
Michael Strunk <mycomycul@gmail.com>
2019-07-26 09:23:20 -0700

    Add Mvc Endpoint

--- scratch/Controllers/HelloWorldController.cs
+++ scratch/Controllers/HelloWorldController.cs
@@ -3,14 +3,34 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace scratch.Controllers
 {
-    public class HelloWorldController
+    /// <summary>
+    /// Core Mvc Provides both Mvc and API endpoints in the same controller
+    /// </summary>
+    public class HelloWorldController:Controller
     {
+        /// <summary>
+        /// Basic Api Endpoint. Doesn't require controller base class
+        /// </summary>
+        /// <returns></returns>
         [HttpGet("api/helloworld")]
-        public object HelloWorld(){
+        public object HelloWorldApi(){
             return new{
                 message = "Hello World",
                 time = DateTime.Now
             };
+        }
+
+        /// <summary>
+        /// Mvc Endpoint. Controller class provides support for Viewbags, Models etc.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("helloworld")]
+        public ActionResult HellowWorldMvc(){
+            ViewBag.Message = "Hello world!";
+            ViewBag.Time = DateTime.Now;
+
+            //return View("~/helloworld.cshtml");
+            return View("helloworld");
         }
     }
 }
